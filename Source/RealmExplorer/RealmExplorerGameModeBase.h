f2e86b9fcc03e5b11567773f33fe8b60a032835e
Anders-T | 0x80.eth <anders.torbjornsen87@gmail.com>
2022-05-03 00:53:26 +0100

    empty project

---
+++ Source/RealmExplorer/RealmExplorerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RealmExplorerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REALMEXPLORER_API ARealmExplorerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
